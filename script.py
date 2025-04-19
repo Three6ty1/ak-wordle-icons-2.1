728643cbaa4d75ff4e089c473f8f1274d08508ff
ashlen <thesadru@gmail.com>
2025-04-19 23:12:02 +0200

    Instead of pids try to go until filesize is almost 1GB

--- script.py
+++ script.py
@@ -1,21 +1,32 @@
 """Standalone python script to download arknights assets and upload them to a branch of the current git repository.
 
-Requires python's requests and .NET 6.0 to be already installed.
+Requires: python 3.9+, python's requests, .NET 6.0, ffmpeg (audio only)
 """
 
 import os
+import re
 import subprocess
 import shutil
 import sys
 import tempfile
+import typing
 import zipfile
 import requests
+import argparse
 
 import json
 
+# parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("filter", nargs="?", choices=["art", "audio"], default="art")
+parser.add_argument("--force", action="store_true")
+args = parser.parse_args()
+name_filter: typing.Callable[[str], bool] = (lambda i: "audio" in i) if args.filter == "audio" else (lambda i: "audio" not in i)
+branch = "cn" if args.filter == "art" else "voice"
+
 # checkout the actual asset branch
-subprocess.run(["git", "fetch", "--depth=1", "origin", "cn:cn"], check=True)
-subprocess.run(["git", "checkout", "cn"], check=True)
+subprocess.run(["git", "fetch", "--depth=1", "origin", f"{branch}:{branch}"], check=True)
+subprocess.run(["git", "checkout", branch], check=True)
 
 # prepare network config
 network_config_url = "https://ak-conf.hypergryph.com/config/prod/official/network_config"
@@ -27,22 +38,25 @@ assets_url = network_urls["hu"] + "/Android/assets/" + res_version + "/"
 
 # figure out new files
 hot_update_list = requests.get(assets_url + "hot_update_list.json").json()
-if os.path.isfile("bundles/hot_update_list.json"):
+to_update: list[dict[str, typing.Any]]
+if os.path.isfile("bundles/hot_update_list.json") and not args.force:
     with open("bundles/hot_update_list.json", encoding="utf-8") as file:
         old_hot_update_list = json.load(file)
 
     old_hot_update_list_by_name = {i["name"]: i for i in old_hot_update_list["abInfos"]}
-    to_update = [i for i in hot_update_list["abInfos"] if old_hot_update_list_by_name[i["name"]]["hash"] != i["hash"]]
+    to_update = [
+        i
+        for i in hot_update_list["abInfos"]
+        if (i["name"] not in old_hot_update_list_by_name or old_hot_update_list_by_name[i["name"]]["hash"] != i["hash"]) and name_filter(i["name"])
+    ]
 else:
-    to_update = hot_update_list["abInfos"]
+    to_update = [i for i in hot_update_list["abInfos"] if name_filter(i["name"])]
 
-if len(to_update) == 0 and not (len(sys.argv) > 1 and sys.argv[1] == "--force"):
+to_update.sort(key=lambda i: (i.get("pid", ""), i["cid"]))
+
+if len(to_update) == 0:
     print("Up to date")
     exit(0)
-
-to_update_grouped_by_pid: dict[str, list[str]] = {}
-for i in to_update:
-    to_update_grouped_by_pid.setdefault(i.get("pid"), []).append(i["name"])
 
 # download ArknightsStudioCLI
 if os.name == "nt":
@@ -65,15 +79,21 @@ if not os.path.isfile(arknights_studio_path):
 os.chmod(arknights_studio_path, 0o775)
 
 # update in groups to ensure enough space is present
-for pid, to_update in to_update_grouped_by_pid.items():
-    print(f"\n\nRunning for pid {pid} ({len(to_update)} files)")
+to_update_index = 0
+while to_update_index < len(to_update):
+    print(f"\n\nStarting at {to_update_index}/{len(to_update) - 1}")
 
-    # download all files
+    # download files and extract them until the update would be too large
     os.makedirs("bundles", exist_ok=True)
     os.makedirs("bundles/tmp", exist_ok=True)
-    for name in to_update:
-        print("download", name)
+    os.makedirs("unstructured_assets", exist_ok=True)
+    while to_update_index < len(to_update):
+        name: str = to_update[to_update_index]["name"]
+        to_update_index += 1
 
+        # download file
+        du = shutil.disk_usage(".")
+        print(f"download {name} ({du.free / 2**30:.1f}GB/{du.total / 2**30:.1f}GB remaining)")
         formatted_name = name.replace("/", "_").replace("#", "__").rsplit(".", 1)[0] + ".dat"
         url = assets_url + formatted_name
         zip_file_path = "bundles/tmp/" + formatted_name
@@ -87,77 +107,90 @@ for pid, to_update in to_update_grouped_by_pid.items():
 
         os.remove(zip_file_path)
 
-    # extract all files to an unstructured output
-    print("Extracting files")
-    unstructured_assets_path = "unstructured_assets"
-    assets_path = "assets"
-    os.makedirs(unstructured_assets_path, exist_ok=True)
-    for root, dirs, files in os.walk("bundles"):
-        for file in files:
-            path = os.path.join(root, file)
-            du = shutil.disk_usage(".")
-            print("extract", os.path.relpath(path, "bundles"), f"({du.free / 2**30:.1f}GB/{du.total / 2**30:.1f}GB remaining)")
-            command = [
-                arknights_studio_path,
-                path,
-                "-g",
-                "containerFull",
-                "-t",
-                "Sprite,AkPortraitSprite,AudioClip,TextAsset",
-                "--log-level",
-                "warning",
-                "-o",
-                os.path.abspath(unstructured_assets_path),
-            ]
-            subprocess.run(command, check=True, stdout=subprocess.DEVNULL)
-            os.remove(path)
+        # extract all found files
+        for root, dirs, files in os.walk("bundles"):
+            for file in files:
+                path = os.path.join(root, file)
+                command = [
+                    arknights_studio_path,
+                    path,
+                    "-g",
+                    "containerFull",
+                    "-t",
+                    "Sprite,AkPortraitSprite,AudioClip,TextAsset",
+                    "--log-level",
+                    "warning",
+                    "-o",
+                    os.path.abspath("unstructured_assets"),
+                ]
+                subprocess.run(command, check=True, stdout=subprocess.DEVNULL)
+                os.remove(path)
+
+        # count up the size of the current files and potentially abort
+        upload_size = 0
+        for dirpath, dirnames, filenames in os.walk("unstructured_assets"):
+            for f in filenames:
+                fp = os.path.join(dirpath, f)
+                # skip broken symlinks
+                if os.path.exists(fp):
+                    upload_size += os.path.getsize(fp)
+
+        # the actual github limit is 2GB but we assume 1GB and try to be safe
+        if upload_size > 900 * 1024 * 1024:
+            break
 
     # move (or overwrite if exists) unstrctured files with the actual assets directory
     # this is to counteract the way duplicates and containerFull behaves
+    # process wav files to mp3 files to compress them a bit (estimated 180KB/s vs 16KB/s)
     print("Structuring and overwriting files")
     # get all directories with files in them
-    for root, dirs, files in os.walk(unstructured_assets_path):
+    for root, dirs, files in os.walk("unstructured_assets"):
         # when a file is alone in its directory then move it up a directory
         # when a file is in a numbered directory (with #) then also move it up a directory
-        if not files:
-            continue
-        if len(files) == 1 or "#" in root:
-            for file in files:
-                current_path = os.path.join(root, file)
-                desired_relpath = os.path.join(os.path.dirname(os.path.dirname(os.path.relpath(current_path, unstructured_assets_path))), file)
-                future_path = os.path.join(assets_path, desired_relpath).lower()
+        # when a file is in the form filename_#00.ext and there exists filename.ext, skip it, as its a duplicate due to assetstudio
+        for file in files:
+            if (match := re.match(r"^(.+?)(_#\d+?)(\..+?)$", file)) and match[1] + match[3] in files:
+                continue
+            current_path = os.path.join(root, file)
+            if len(files) == 1 or "#" in root:
+                desired_relpath = os.path.join(os.path.dirname(os.path.dirname(os.path.relpath(current_path, "unstructured_assets"))), file)
+                future_path = os.path.join("assets", desired_relpath).lower()
+            else:
+                future_path = os.path.join("assets", os.path.relpath(current_path, "unstructured_assets").lower())
 
-                os.makedirs(os.path.dirname(future_path), exist_ok=True)
+            os.makedirs(os.path.dirname(future_path), exist_ok=True)
+            if os.path.splitext(current_path)[1] == ".wav":
+                subprocess.run(["ffmpeg", "-i", current_path, os.path.splitext(future_path)[0] + ".mp3"], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+                os.remove(current_path)
+            else:
                 shutil.move(os.path.abspath(current_path), os.path.abspath(future_path))
-        else:
-            for file in files:
-                current_path = os.path.join(root, file)
-                future_path = os.path.join(assets_path, os.path.relpath(current_path, unstructured_assets_path).lower())
 
-                os.makedirs(os.path.dirname(future_path), exist_ok=True)
-                shutil.move(os.path.abspath(current_path), os.path.abspath(future_path))
-
-    # clean up to ensure enough space is left for other 'pid's
+    # clean up to ensure enough space is left
     print("Cleaning up")
     shutil.rmtree("bundles")
-    shutil.rmtree(unstructured_assets_path)
+    shutil.rmtree("unstructured_assets")
+    subprocess.run(["du", "-h", "--threshold=1G", "."])
 
     # push segment to git
     print("Pushing to git")
     sys.stdout.flush()
     subprocess.run(["git", "add", "assets"], check=True)
-    subprocess.run(["git", "status"], check=True)
-    subprocess.run(["git", "commit", "-m", f"update {res_version} part {pid or 'null'}"], check=True)
+    if not subprocess.check_output(["git", "diff", "--cached", "--name-only", "assets"]).strip():
+        break
+    comp = subprocess.run(["git", "commit", "-m", f"update {res_version} segment {to_update_index}/{len(to_update)}"], capture_output=True)
+    if comp.returncode != 0:
+        print(comp.stdout, comp.stderr) # TODO: sometimes this fails and I do not understand why
+        comp.check_returncode()
     subprocess.run(["git", "log", "--oneline"], check=True)
-    subprocess.run(["git", "push", "origin", "cn"], check=True)
+    subprocess.run(["git", "push", "origin", branch], check=True)
 
     # clean up git commits
     sys.stdout.flush()
     print("Cleaning up git")
     sha = subprocess.check_output(["git", "rev-parse", "HEAD"]).decode().strip()
     subprocess.run(["git", "checkout", "--detach", sha], check=True)
-    subprocess.run(["git", "fetch", "--depth=1", "origin", "cn:cn"], check=True)
-    subprocess.run(["git", "checkout", "cn"], check=True)
+    subprocess.run(["git", "fetch", "--depth=1", "origin", f"{branch}:{branch}"], check=True)
+    subprocess.run(["git", "checkout", branch], check=True)
     subprocess.run(["git", "reflog", "expire", "--expire=now", "--all"], check=True)
     subprocess.run(["git", "gc", "--prune=now"], check=True)
 
@@ -168,6 +201,6 @@ with open("bundles/hot_update_list.json", "w", encoding="utf-8") as file:
 
 # push final file to git
 subprocess.run(["git", "add", "bundles"], check=True)
-subprocess.run(["git", "commit", "-m", f"update {res_version} part hot_update_list"], check=True)
-subprocess.run(["git", "push", "origin", "cn"], check=True)
+subprocess.run(["git", "commit", "-m", f"update {res_version} segment hot_update_list"], check=True)
+subprocess.run(["git", "push", "origin", branch], check=True)
 subprocess.run(["git", "checkout", "master"], check=True)
